7191c9d0b293a7f9ab2aa8be118fe73c599ddc21
Mayr Di Franco <mayraroucha09@gmail.com>
2023-03-10 16:06:03 -0300

    calculo idade

---
+++ src/idade_calculo/IdadeCalculo.java
@@ -0,0 +1,14 @@
+package idade_calculo;
+
+public class IdadeCalculo {
+
+	public static void main(String[] args) {
+		int anoAtual = 2023;
+		int anoNascimento = 1994;
+		int idadeAtual = anoAtual - anoNascimento;
+		
+		System.out.println("A sua idade atual é: " + idadeAtual);
+		
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Mayr Di Franco
+ *
+ */
+module idade_calculo {
+}
